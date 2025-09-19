013107bec167d9faf06ee8c3821f4c4524980261
kranthi <donadikranthikumar@gmail.com>
2025-09-19 11:11:54 +0530

    main

--- shell-practice/Variable.sh
+++ shell-practice/Variable.sh
@@ -3,7 +3,7 @@
 #no space here or after equal
 PERSON1=John
 PERSON2=Raheem
-echo "PERSON1:: Hi PERSON2, How are you?"
-echo "PERSON2:: Hello PERSON1, I am fine."
-echo "PERSON1:: Where are you working?"
-echo "PERSON2:: I'm  working at TCS with Linux Domain"
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine."
+echo "$PERSON1:: Where are you working?"
+echo "$PERSON2:: I'm  working at TCS with Linux Domain"
